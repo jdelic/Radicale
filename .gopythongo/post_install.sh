87f1cc697c3e3255a8e0715c75aa017b977fb9b5
Jonas Maurus <jonas-github@maurus.net>
2017-01-21 12:16:06 +0100

    add preremove scripts, fix typo, use searchfile versioner

--- .gopythongo/post_install.sh
+++ .gopythongo/post_install.sh
@@ -3,19 +3,21 @@
 # add the radicale user and group if it doesn't exist yet
 adduser --home /run/radicale --disabled-login --disabled-password --system --group radicale
 
+systemctl --system daemon-reload >/dev/null || true
+
 # the following was assembled from various default blocks created by dh_make helpers
 # in packages using the default deb build system
 if [ -x "/usr/bin/deb-systemd-helper" ]; then
     # unmask if previously masked by apt-get remove
     /usr/bin/deb-systemd-helper unmask radicale.service >/dev/null || true
 
-    if /usr/bin/deb-systemd-helper --quiet was-enabled radicale.service; then
-        # Enables the unit on first installation, creates new
-        # symlinks on upgrades if the unit file has changed.
-        deb-systemd-helper enable radicale.service >/dev/null || true
+    if /usr/bin/deb-systemd-helper --quiet is-enabled radicale.service; then
+        # If radicale had been installed before restart it (upgrade)
+        deb-systemd-helper reenable radicale.service >/dev/null || true
+        deb-systemd-invoke restart radicale >/dev/null || true
     else
-        # Update the statefile to add new symlinks (if any), which need to be
-        # cleaned up on purge. Also remove old symlinks.
+        # on first install, disable. The admin will enable and start the service.
+        deb-systemd-helper disable radicale.service > /dev/null || true
         deb-systemd-helper update-state radicale.service >/dev/null || true
     fi
 fi
